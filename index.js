4460a7b1fc82a68aa30cca3d1ae0e57f24474e04
Nilesh <Nilesh@example.com>
2023-09-23 10:37:51 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides[currentSlide].classList.remove("active-slide");
+  currentSlide = index;
+  slides[currentSlide].classList.add("active-slide");
+}
+
+function nextSlide() {
+  showSlide((currentSlide + 1) % slides.length);
+}
+
+function prevSlide() {
+  showSlide(currentSlide === 0 ? slides.length - 1 : currentSlide - 1);
+}
+
+setInterval(nextSlide, 3000);
+
+showSlide(currentSlide);
+///////////////////////////////////////////////////////////////
+const slider = document.querySelector(".slider");
+let scrollInterval;
+
+function scrollRight() {
+  const firstSlide = slider.firstElementChild;
+  slider.style.transition = "none";
+  slider.appendChild(firstSlide);
+  slider.style.transform = "translateX(25%)";
+  setTimeout(() => {
+    slider.style.transition = "transform 0.8s ease-in-out";
+    slider.style.transform = "translateX(0)";
+  }, 0);
+}
+
+function autoScroll() {
+  scrollInterval = setInterval(scrollRight, 5000); 
+}
+
+autoScroll();
+
+////////////////////////////////////////////
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
